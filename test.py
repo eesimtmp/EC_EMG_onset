cfe3de3de0ea51a12eed5f71bc2537a1287996bf
eesimtmp <126083032+eesimtmp@users.noreply.github.com>
2024-04-10 21:02:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 tremogram = pd.read_csv('./test.csv')
 signal = tremogram[tremogram.columns[1]].to_numpy()
 time = tremogram['Time'].to_numpy()
-print(1/(time[1]-time[0]))
 
 #output = EMG_to_binary_signal(signal, time, [0.3,0.5], Fs=5000, ma_dur=40E-3, thr_sig=2000, bp_filt=True, bp_filt_low=10, bp_filt_high=300)
 
